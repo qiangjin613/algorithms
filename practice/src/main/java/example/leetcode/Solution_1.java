49078e9a1a9a35814a1afb61c50674f1af8d299f
qiangjin <qiangjin613@163.com>
2023-02-19 18:58:10 +0800

    feat(力扣): #1, #26

---
+++ practice/src/main/java/example/leetcode/Solution_1.java
@@ -0,0 +1,40 @@
+package example.leetcode;
+
+import java.util.HashMap;
+
+public class Solution_1 {
+    /**
+     * 暴力扫描
+     */
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] {i ,j};
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 使用哈希表进行优化，对数据进行缓存，减少一次循环
+     * <p>
+     * Note: 可以在一个循环中进行查找和构建哈希表，是应为 c - a = b 等价于 c - b = a。
+     */
+    public int[] twoSum2(int[] nums, int target) {
+        HashMap<Integer, Integer> hashTable = new HashMap<>(nums.length);
+        for (int i = 0; i < nums.length; i++) {
+           if (hashTable.containsKey(target - nums[i])) {
+               return new int[] {i, hashTable.get(target - nums[i])};
+           } else {
+               hashTable.put(nums[i], i);
+           }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        new Solution_1().twoSum2(new int[] {2,7,11,15}, 9);
+    }
+}
---
+++ practice/src/main/java/example/leetcode/Solution_26.java
@@ -0,0 +1,26 @@
+package example.leetcode;
+
+public class Solution_26 {
+    /**
+     * 双指针
+     * <ul>
+     * <li> 快指针：扫描数组，查找非重复的元素
+     * <li> 慢指针：慢指针的下一个位置可覆盖的位置
+     * </ul>
+     */
+    public int removeDuplicates(int[] nums) {
+        int slow, fast;
+        for (slow = 0, fast = slow + 1; fast < nums.length;) {
+            if (nums[slow] == nums[fast]) {
+                fast++;
+            } else {
+                nums[++slow] = nums[fast];
+            }
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution_26().removeDuplicates(new int[]{1, 1, 2}));
+    }
+}
