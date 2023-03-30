22be1da6af7f48a24bc04ff0c1563cf44cad37be
qiangjin <qiangjin613@163.com>
2023-03-30 22:58:24 +0800

    feat(力扣): 455

---
+++ practice/src/main/java/example/leetcode/Solution_455.java
@@ -0,0 +1,33 @@
+package example.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * <a href="https://leetcode.cn/problems/assign-cookies/">455. 分发饼干</a>
+ */
+public class Solution_455 {
+
+    /**
+     * 排序 + 双指针
+     */
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int count = 0;
+        for (int i = 0, j = 0; i < g.length && j < s.length; ) {
+            if (g[i] <= s[j]) {
+                i++;
+                j++;
+                count++;
+            } else {
+                j++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution_455().findContentChildren(new int[] {1,2,2}, new int[] {1,2,3}));
+    }
+}
