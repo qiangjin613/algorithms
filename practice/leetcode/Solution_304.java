6704c8f079f0e5b97420abbbf07a527f39eea0f1
qiangjin <qiangjin613@163.com>
2023-08-08 00:37:50 +0800

    feat(力扣): 304

---
+++ practice/leetcode/Solution_304.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+import javax.swing.*;
+
+/**
+ * <a href = "https://leetcode.cn/problems/range-sum-query-2d-immutable/">304. 二维区域和检索 - 矩阵不可变</a>
+ */
+public class Solution_304 {
+
+    class NumMatrix {
+
+        private int[][] preSum;
+
+        public NumMatrix(int[][] matrix) {
+            int m = matrix.length, n = matrix[0].length;
+            if (m == 0 || n == 0) {
+                return;
+            }
+            // 构造前缀和二位数组
+            preSum = new int[m + 1][n + 1];
+            for (int i = 1; i <= m; i++) {
+                for (int j = 1; j <= n; j++) {
+                    preSum[i][j] = preSum[i - 1][j] + preSum[i][j - 1] - preSum[i - 1][j - 1] + matrix[i - 1][j - 1];
+                }
+            }
+        }
+
+        public int sumRegion(int row1, int col1, int row2, int col2) {
+            return preSum[row2 + 1][col2 + 1] - preSum[row2 + 1][col1] - preSum[row1][col2 + 1] + preSum[row1][col1];
+        }
+    }
+
+}
