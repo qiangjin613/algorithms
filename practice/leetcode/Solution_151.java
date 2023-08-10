0b1cbe46fd5472feaf2dc34f1c130527d966023d
qiangjin <qiangjin613@163.com>
2023-08-11 01:30:03 +0800

    feat(力扣): 48、151

---
+++ practice/leetcode/Solution_151.java
@@ -0,0 +1,46 @@
+package leetcode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * <a href = "https://leetcode.cn/problems/reverse-words-in-a-string/">151. 反转字符串中的单词</a>
+ */
+public class Solution_151 {
+
+    public String reverseWords(String s) {
+        String[] strArray = s.split(" ");
+        LinkedList<String> words = new LinkedList<>();
+        for (int i = 0; i < strArray.length; i++) {
+            if ("".equals(strArray[i])) {
+                continue;
+            }
+            words.addFirst(strArray[i]);
+        }
+        return String.join(" ", words);
+    }
+
+    public String reverseWords_2(String s) {
+        // 特殊情况转为统一处理，如果没有这个处理，可以使用 “Hello   World  ” 作为测试用例试试看会发生什么
+        s = ' ' + s;
+        StringBuilder res = new StringBuilder();
+        for (int right = s.length(), left = right - 1; left >= 0; left--) {
+            // 遇到空格，说明获得一个完整单词
+            if (s.charAt(left) == ' ') {
+                // 防止连续空格的情况
+                if (left + 1 < right) {
+                    res.append(s, left + 1, right).append(' ');
+                }
+                // 让right紧跟left，避免连续多个空格被甩开，导致添加单词时候里面有多余空格
+                right = left;
+            }
+        }
+        //结尾处理一下多余空格
+        return res.substring(0, res.length() - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution_151().reverseWords("  hello    world  "));
+        System.out.println(new Solution_151().reverseWords_2("  hello    world  "));
+    }
+}
---
+++ practice/leetcode/Solution_48.java
@@ -0,0 +1,61 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * <a href = "https://leetcode.cn/problems/rotate-image/">48. 旋转图像</a>
+ */
+public class Solution_48 {
+
+    /**
+     * 空间复杂度 O(n)
+     */
+    public void rotate(int[][] matrix) {
+        int[][] dummy = new int[matrix.length][matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                dummy[j][matrix[i].length - i - 1] = matrix[i][j];
+            }
+        }
+        for (int i = 0; i < dummy.length; i++) {
+            for (int j = 0; j < dummy[i].length; j++) {
+                matrix[i][j] = dummy[i][j];
+            }
+        }
+    }
+
+    /**
+     * 空间复杂度 O(1)
+     */
+    public void rotate2(int[][] matrix) {
+        // 对称轴反转
+        for (int i = 0; i < matrix.length ; i++) {
+            for (int j = i; j < matrix[i].length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        // 每行反转
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix[i].length - 1 - j];
+                matrix[i][matrix[i].length - 1 - j] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}};
+        new Solution_48().rotate(matrix);
+        for (int i = 0; i < matrix.length; i++) {
+            System.out.println(Arrays.toString(matrix[i]));
+        }
+        int[][] matrix2 = {{1, 2, 3, 4}, {5, 6, 7, 8}, {7, 6, 5, 4}, {4, 1, 2, 3}};
+        new Solution_48().rotate2(matrix2);
+        for (int i = 0; i < matrix2.length; i++) {
+            System.out.println(Arrays.toString(matrix2[i]));
+        }
+    }
+}
