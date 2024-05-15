2acc1f5c6a3625cd8c4d7df0c8bf080bc508ae45
qiangjin <qiangj@bsfit.com.cn>
2024-05-15 15:09:47 +0800

    feat(力扣): 67

---
+++ practice/leetcode/Solution_67.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+/**
+ * <a href="https://leetcode.cn/problems/add-binary/description/">67. 二进制求和</a>
+ */
+public class Solution_67 {
+
+    public String addBinary(String a, String b) {
+        StringBuilder result = new StringBuilder();
+        // 进位标志
+        int flag = 0;
+        for (int indexA = a.length() - 1, indexB = b.length() - 1; indexA >= 0 || indexB >= 0 || flag != 0; indexA--, indexB--) {
+            int curA = 0, curB = 0;
+            if (indexA >= 0) {
+                curA = a.charAt(indexA) - '0';
+            }
+            if (indexB >= 0) {
+                curB = b.charAt(indexB) - '0';
+            }
+
+            int temp = curA + curB + flag;
+
+            result.append(temp % 2);
+            flag = temp / 2;
+        }
+        return result.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution_67().addBinary("0", "0"));
+    }
+}
