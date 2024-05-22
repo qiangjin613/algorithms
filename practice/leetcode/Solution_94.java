aaaf9819bf735ee3882c6629b6f983b519ad3423
qiangjin <qiangjin613@163.com>
2024-05-22 17:15:48 +0800

    feat(力扣): 94

---
+++ practice/leetcode/Solution_94.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+import leetcode.structure.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution_94 {
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        inorder(root, res);
+        return res;
+    }
+
+    private void inorder(TreeNode root, List<Integer> res) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left, res);
+        res.add(root.val);
+        inorder(root.right, res);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1, null, new TreeNode(2, new TreeNode(3), null));
+        System.out.println(new Solution_94().inorderTraversal(root));
+    }
+}
